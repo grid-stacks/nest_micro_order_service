086dcc8dadd7feba4467523ae315a78ce01edb24
DHN Chandan <php.chandan@gmail.com>
2022-04-22 00:09:26 +0600

    Update auth module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'nest_micro_order',
+      username: 'postgres',
+      password: '10091986',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
