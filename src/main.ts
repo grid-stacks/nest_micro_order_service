9c32a6ce94872c78798db934acc64c7ced36471a
DHN Chandan <php.chandan@gmail.com>
2022-04-23 02:02:42 +0600

    Add grpc exception filter

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { protobufPackage } from './order/order.pb';
+import { HttpExceptionFilter } from './order/filter/http-exception.filter';
 
 async function bootstrap() {
   const app: INestMicroservice = await NestFactory.createMicroservice(
@@ -18,6 +19,7 @@ async function bootstrap() {
     },
   );
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen();
